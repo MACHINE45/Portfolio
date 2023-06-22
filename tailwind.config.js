f0ea6fe3f3f0e0a2fbe80e9f2e1bbd556500170a
VINEET KUMAR CHAUBEY <chaubeyvineet48@gmail.com>
2023-06-22 17:29:17 +0530

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        blue: "#2CBCE9",
+        red: "DC4492",
+        yellow: "#ededed",
+        "deep-blue": "#010026",
+        "dark-grey": "#757575",
+        "opaque-black":"rgba(0,0,0,0.35)"
+      },
+      backgroundImage:
+    },
+  },
+  plugins: [],
+}
+
