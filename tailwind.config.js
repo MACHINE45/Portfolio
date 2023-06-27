1890c8e272a823f2c9f8606485a18eebb6c9a34a
VINEET KUMAR CHAUBEY <chaubeyvineet48@gmail.com>
2023-06-27 16:11:16 +0530

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "deep-blue": "#010026",
         blue: "#2CBCE9",
         red: "#DC4492",
         yellow: "#FDCC49",
         grey: "#ededed",
+        "deep-blue": "#010026",
         "dark-grey": "#757575",
-        "opaque-black": "rgba(0,0,0,0.35)",
+        "opaque-black": "rgba(0,0,0,0.35)"
       },
       backgroundImage: (theme) => ({
         "gradient-rainbow":
