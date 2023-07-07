10dcc769058eedaed514a995c0ce060cf1b20841
VINEET KUMAR CHAUBEY <93907642+MACHINE45@users.noreply.github.com>
2023-07-07 22:55:33 +0530

    Update Navbar.jsx

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -2,24 +2,23 @@ import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery";
 
-const Link = ({ page, selectedPage, setSelectedPage})=>{
+const Link = ({ page, selectedPage, setSelectedPage }) => {
     const lowerCasePage = page.toLowerCase();
     return (
         <AnchorLink
             className={`${selectedPage === lowerCasePage ? "text-yellow" : ""}
                 hover:text-yellow transition duration-500`}
             href={`#${lowerCasePage}`}
-            onClick={()=> setSelectedPage(lowerCasePage)}
+            onClick={() => setSelectedPage(lowerCasePage)}
         >
             {page}
         </AnchorLink>
     )
 }
-
-const Navbar=({ selectedPage, setSelectedPage})=>{
-    const [isMenuToggled, setIsMenuToggled]=useState(false);
+const Navbar = ({ selectedPage, setSelectedPage }) => {
+    const [isMenuToggled, setIsMenuToggled] = useState(false);
+    const isDesktop = useMediaQuery("(min-width: 768px)");
     const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
-
     return (
         <nav className={`z-40 w-full fixed top-0 py-6`}>
             <div className="flex items-center justify-between mx-auto w-5/6">
@@ -28,44 +27,83 @@ const Navbar=({ selectedPage, setSelectedPage})=>{
                 {/* DESKTOP NAV*/}
                 {isAboveSmallScreens ? (
                     <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
-                        <Link 
-                        page="Home"
-                        selectedPage={selectedPage}
-                        setSelectedPage={setSelectedPage}
+                        <Link
+                            page="Home"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
                         />
-                        <Link 
-                        page="Skills"
-                        selectedPage={selectedPage}
-                        setSelectedPage={setSelectedPage}
+                        <Link
+                            page="Skills"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
                         />
-                        <Link 
-                        page="Projects"
-                        selectedPage={selectedPage}
-                        setSelectedPage={setSelectedPage}
+                        <Link
+                            page="Projects"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
                         />
-                        <Link 
-                        page="Testimonials"
-                        selectedPage={selectedPage}
-                        setSelectedPage={setSelectedPage}
+                        <Link
+                            page="Testimonials"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
                         />
-                        <Link 
-                        page="Contact"
-                        selectedPage={selectedPage}
-                        setSelectedPage={setSelectedPage}
+                        <Link
+                            page="Contact"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
                         />
                     </div>
                 ) : (
                     <button
-                    className="rounded-full bg-red p-2"
-                    onClick={()=> setIsMenuToggled(!isMenuToggled)}
+                      className="rounded-full bg-red p-2"
+                      onClick={() => setIsMenuToggled(!isMenuToggled)}
                     >
-                        <image alt="menu-icon" src="../assets/menu-icon.svg" />
+                      <img alt="menu-icon" src="../assets/menu-icon.svg" />
                     </button>
+                  )}
+                {/* MOBILE MENU POPUP */}
+           
+                { !isDesktop && isMenuToggled && (
+                    <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+                        {/* CLOSE ICON */}
+                        <div className="flex justify-end p-12">
+                            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                                <img alt="close-icon" src="../assets/close-icon.svg" />
+                            </button>
+                        </div>
+
+                        {/* MENU ITEMS */}
+                        <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+                            <Link
+                                page="Home"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                            <Link
+                                page="Skills"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                            <Link
+                                page="Projects"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                            <Link
+                                page="Testimonials"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                            <Link
+                                page="Contact"
+                                selectedPage={selectedPage}
+                                setSelectedPage={setSelectedPage}
+                            />
+                        </div>
+                    </div>
                 )}
             </div>
         </nav>
     );
 };
-
-
 export default Navbar;
